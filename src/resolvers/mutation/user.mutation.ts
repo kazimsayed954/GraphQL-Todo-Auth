1842c3f1feaf5020aa67581410c6d2896bcb475b
kazim.sayed <kazim.sayed@neosoftmail.com>
2024-02-06 10:35:38 +0530

    added validation in file type

--- src/resolvers/mutation/user.mutation.ts
+++ src/resolvers/mutation/user.mutation.ts
@@ -1,25 +1,34 @@
 import { loginUser, registerUser, uploadProfile } from "../../services/user.service";
-import { User } from '../../utils/types/user.type'
+import { User } from '../../utils/types/user.type';
 import { handleContextError } from "../../utils/contextHandler";
+import { GraphQLError } from "graphql";
 
 const UserMutation = {
-    register: async (_:any,args:{user:User}) => {
+    register: async (_: any, args: { user: User; }) => {
         const data = await registerUser(args?.user);
         return data;
     },
-    login: async (_:any,args:{user:User}) => {
+    login: async (_: any, args: { user: User; }) => {
         const data = await loginUser(args?.user);
         return data;
     },
-    uploadProfilePic: async (_:any,args:any,context:any) => {
+    uploadProfilePic: async (_: any, args: any, context: any) => {
         if (context && context.error) {
-            return handleContextError(context.error)
+            return handleContextError(context.error);
         }
-        const {file} = args;
-        const data = await uploadProfile(context?._id,file.file);
+        const { file } = args;
+        const data = await uploadProfile(context?._id, file.file);
+        if (data?.error) {
+            throw new GraphQLError(data?.error, {
+                extensions: {
+                    code: data?.code,
+                    http: { status: data?.status },
+                },
+            });
+        }
         return data;
-        
+
     }
-}
+};
 
 export default UserMutation;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,53 +1,64 @@
 import * as argon from "argon2";
 import UserModel from "../models/User.model";
-import { User } from '../utils/types/user.type'
+import { User } from '../utils/types/user.type';
 import jwt from "jsonwebtoken";
 import fs from "fs";
 import path from "path";
 import ProfileImageModel from "../models/ProfileImage.model";
 import MyLib from "../utils/generateUID";
+import mime from 'mime-types';
 
-export async function registerUser(user:User){
-    const {fullName,email,password} = user;
+export async function registerUser(user: User) {
+    const { fullName, email, password } = user;
     try {
         const hashedPassword = await argon.hash(password);
-        const user = await UserModel.create({fullName,email,password:hashedPassword});
-        return user;       
+        const user = await UserModel.create({ fullName, email, password: hashedPassword });
+        return user;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-export async function loginUser(user:User){
+export async function loginUser(user: User) {
     try {
-        const {email,password} = user;
-        const userData = await UserModel.findOne({email});
-        if(!userData) throw new Error("Not Found User");
-        const isPasswordMatch = await argon.verify(userData.password,password);
-        if(!isPasswordMatch) throw new Error("Wrong Password Provided");
-        const jwtToken  = jwt.sign({id:userData.id},process.env.JWT_SECRET as string,{expiresIn:'12h'});
-        const {  password:pwd, ...rest } = userData;   
-        return {rest,token:jwtToken}
-                       
+        const { email, password } = user;
+        const userData = await UserModel.findOne({ email });
+        if (!userData) throw new Error("Not Found User");
+        const isPasswordMatch = await argon.verify(userData.password, password);
+        if (!isPasswordMatch) throw new Error("Wrong Password Provided");
+        const jwtToken = jwt.sign({ id: userData.id }, process.env.JWT_SECRET as string, { expiresIn: '12h' });
+        const { password: pwd, ...rest } = userData;
+        return { rest, token: jwtToken };
+
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-export async function uploadProfile(userId:string,file:any){
+export async function uploadProfile(userId: string, file: any) {
     const baseUrl = 'http://localhost:7000';
     try {
         const rootDirectory = process.cwd();
-        const { createReadStream, filename, mimetype, encoding } = await file;
+        const { createReadStream, filename, encoding } = await file;
+        const mimetype = mime.lookup(filename);
+        // Validate image type
+        if (!mimetype || !mimetype.startsWith('image/')) {
+            return {
+                error: "Invalid image type. Only image files are allowed",
+                code: "INVALID",
+                status: 422
+            };
+        }
+
         const stream = createReadStream();
         const filenameId = MyLib.generateUid();
-        const uniqueFileName = `${filenameId}${filename}`
+        const uniqueFileName = `${filenameId}${filename}`;
         const pathName = path.join(rootDirectory, 'public', 'images', uniqueFileName);
         await stream.pipe(fs.createWriteStream(pathName));
         const profilePic = `${baseUrl}/images/${uniqueFileName}`;
-        let profile:any = null;
-        profile = await ProfileImageModel.findOne({userId });
-        if(profile){
+        let profile: any = null;
+        profile = await ProfileImageModel.findOne({ userId });
+        if (profile) {
 
             const existingImagePath = path.join(rootDirectory, 'public', 'images', profile.url.split('/').pop());
             fs.unlink(existingImagePath, (err) => {
@@ -55,20 +66,20 @@ export async function uploadProfile(userId:string,file:any){
                     console.error('Error deleting existing image file:', err);
                 }
             });
-            await ProfileImageModel.findOneAndUpdate({userId},{url:profilePic})
+            await ProfileImageModel.findOneAndUpdate({ userId }, { url: profilePic });
             return {
                 profilePic
-            }
+            };
         }
-        const data = await ProfileImageModel.create({url:profilePic,userId})
+        const data = await ProfileImageModel.create({ url: profilePic, userId });
         await UserModel.findOneAndUpdate({ _id: userId }, { profileId: data?._id }, { new: true });
-        
+
         return {
             profilePic
-        }
-        
+        };
+
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
